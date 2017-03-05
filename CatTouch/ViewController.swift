5761805e56d6d51bb0bd2cf52f2e8b951137fed4
Ivan Phytsyk <ivanphytsyk@Ivans-MacBook-Pro.local>
2017-03-05 12:52:30 +0200

    Initial Commit

---
+++ CatTouch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CatTouch
+//
+//  Created by Ivan Phytsyk on 05.03.17.
+//  Copyright © 2017 Ivan Phytsyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
